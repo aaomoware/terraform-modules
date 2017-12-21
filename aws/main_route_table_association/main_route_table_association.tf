f66f822da9f74d5111da562e58adce1dc1908fc3
Aghogho A Omoware <a.omoware@comicrelief.com>
2017-12-21 16:38:19 +0000

    with variables

--- aws/main_route_table_association/main_route_table_association.tf
+++ aws/main_route_table_association/main_route_table_association.tf
@@ -1,4 +1,4 @@
 resource "aws_main_route_table_association" "main_rt_assoc" {
-  vpc_id         = "${aws_vpc.foo.id}"
-  route_table_id = "${aws_route_table.bar.id}"
+  vpc_id         = "${var.vpc_id}"
+  route_table_id = "${var.route_table_id}"
 }
