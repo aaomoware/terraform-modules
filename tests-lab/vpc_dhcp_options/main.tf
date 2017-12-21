1081361e14c72471ce600932cbaf9363b4dffc47
Aghogho A Omoware <a.omoware@comicrelief.com>
2017-12-21 12:14:44 +0000

    vpc_dhcp_options tested

---
+++ tests-lab/vpc_dhcp_options/main.tf
@@ -0,0 +1,49 @@
+module "dns" {
+  source = "../../vpc_dhcp_options"
+
+  name                 = "dns"
+  domain_name          = "on.venus"
+  domain_name_servers  = ["192.168.1.5","192.168.1.6"]
+}
+
+module "ntp" {
+  source = "../../vpc_dhcp_options"
+
+  name                 = "ntp"
+  ntp_servers          = ["192.168.1.4"]
+}
+
+module "netbios" {
+  source = "../../vpc_dhcp_options"
+
+  name                 = "netbios"
+  netbios_name_servers = ["192.168.1.3"]
+}
+
+module "ntp_dns" {
+  source = "../../vpc_dhcp_options"
+
+  name                 = "ntp_dns"
+  ntp_servers          = ["192.168.1.4"]
+  domain_name_servers  = ["192.168.1.5","192.168.1.6"]
+}
+
+module "ntp_dns_netbios" {
+  source = "../../vpc_dhcp_options"
+
+  name                 = "ntp_dns_netbios"
+  ntp_servers          = ["192.168.1.4"]
+  domain_name_servers  = ["192.168.1.5","192.168.1.6"]
+  netbios_name_servers = ["192.168.1.3","192.168.1.113"]
+}
+
+module "all_options" {
+  source = "../../vpc_dhcp_options"
+
+  name                 = "all_options"
+  domain_name          = "on.venus"
+  ntp_servers          = ["192.168.1.4"]
+  netbios_node_type    = 2
+  domain_name_servers  = ["192.168.1.5","192.168.1.6"]
+  netbios_name_servers = ["192.168.1.3","192.168.1.113"]
+}
