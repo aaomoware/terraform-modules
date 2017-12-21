ade764f3bec73af0b2da56e8014c0c9c9abb51ea
Aghogho A Omoware <a.omoware@comicrelief.com>
2017-12-21 16:46:59 +0000

    Default VPC - name

--- aws/default_vpc/default_vpc.tf
+++ aws/default_vpc/default_vpc.tf
@@ -1,4 +1,4 @@
-resource "aws_default_vpc" "default" {
+resource "aws_default_vpc" "default_vpc" {
   enable_dns_support   = "${var.enable_dns_support}"
   enable_classiclink   = "${var.enable_classiclink}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
