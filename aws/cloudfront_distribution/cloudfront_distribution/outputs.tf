ffbf366d0bbb000e52bce568a4558e3ad8f9be6c
Aghogho A Omoware <a.omoware@comicrelief.com>
2018-01-10 15:15:53 +0000

    ignore testing dir for now; fix typo

--- aws/cloudfront_distribution/cloudfront_distribution/outputs.tf
+++ aws/cloudfront_distribution/cloudfront_distribution/outputs.tf
@@ -1,18 +1,18 @@
 output "cdn_id" {
-  value = "${resource "aws_cloudfront_distribution.s3_distribution.id}"
+  value = "${aws_cloudfront_distribution.s3_distribution.id}"
 }
 output "cdn_arn" {
-  value = "${resource "aws_cloudfront_distribution.s3_distribution.arn}"
+  value = "${aws_cloudfront_distribution.s3_distribution.arn}"
 }
 output "cdn_domain_name" {
-  value = "${resource "aws_cloudfront_distribution.s3_distribution.domain_name}"
+  value = "${aws_cloudfront_distribution.s3_distribution.domain_name}"
 }
 output "cdn_hosted_zone_id" {
-  value = "${resource "aws_cloudfront_distribution.s3_distribution.hosted_zone_id}"
+  value = "${aws_cloudfront_distribution.s3_distribution.hosted_zone_id}"
 }
 output "cdn_caller_reference" {
-  value = "${resource "aws_cloudfront_distribution.s3_distribution.cdn_caller_reference}"
+  value = "${aws_cloudfront_distribution.s3_distribution.cdn_caller_reference}"
 }
 output "cdn_active_trusted_signers" {
-  value = "${resource "aws_cloudfront_distribution.s3_distribution.active_trusted_signers}"
+  value = "${aws_cloudfront_distribution.s3_distribution.active_trusted_signers}"
 }
