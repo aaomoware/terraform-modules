4f0319bcb539a8ef06f4a0c0ad7d1ce9b208e89d
Aghogho A Omoware <a.omoware@comicrelief.com>
2018-01-10 10:28:23 +0000

    its a list

--- aws/security_group_rule/security_group_rule.tf
+++ aws/security_group_rule/security_group_rule.tf
@@ -1,14 +1,14 @@
 resource "aws_security_group_rule" "rule" {
-  type            = "${var.type}"
-  selF            = "${var.selF}"
-  to_port         = "${var.to_port}"
-  protocol        = "${var.protocol}"
-  from_port       = "${var.from_port}"
-  description     = "${var.description}"
-  cidr_blocks     = ["${var.cidr_blocks}"]
-  prefix_list_ids = ["${var.prefix_list_ids}"]
+  type             = "${var.type}"
+  selF             = "${var.selF}"
+  to_port          = "${var.to_port}"
+  protocol         = "${var.protocol}"
+  from_port        = "${var.from_port}"
+  description      = "${var.description}"
+  cidr_blocks      = ["${var.cidr_blocks}"]
+  prefix_list_ids  = ["${var.prefix_list_ids}"]
+  ipv6_cidr_blocks = ["${var.ipv6_cidr_blocks}"]
 
-  ipv6_cidr_blocks = "${var.ipv6_cidr_blocks}"
   security_group_id = "${var.security_group_id}"
   source_security_group_id = "${var.source_security_group_id}"
 }
