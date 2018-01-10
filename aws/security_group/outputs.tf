e3da624f8b49fd57ba8054afc51e4af2e57f051c
Aghogho A Omoware <a.omoware@comicrelief.com>
2018-01-10 10:40:25 +0000

    sg skeleton outputs; README.md updated

--- aws/security_group/outputs.tf
+++ aws/security_group/outputs.tf
@@ -4,3 +4,10 @@ output "name_sg_id" {
 output "name_prefix_sg_id" {
   value = "${aws_security_group.name_prefix.id}"
 }
+
+output "name_prefix_skeleton_id" {
+  value = "${aws_security_group.name_prefix_skeleton_id.id}"
+}
+output "name_skeleton_id" {
+  value = "${aws_security_group.name_skeleton_id.id}"
+}
