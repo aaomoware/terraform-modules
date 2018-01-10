368c8946f64ff43c008e4e6d11b5492cf73031b9
Aghogho A Omoware <a.omoware@comicrelief.com>
2018-01-10 10:42:43 +0000

    order

--- aws/security_group/outputs.tf
+++ aws/security_group/outputs.tf
@@ -5,9 +5,9 @@ output "name_prefix_sg_id" {
   value = "${aws_security_group.name_prefix.id}"
 }
 
-output "name_prefix_skeleton_id" {
-  value = "${aws_security_group.name_prefix_skeleton_id.id}"
-}
 output "name_skeleton_id" {
   value = "${aws_security_group.name_skeleton_id.id}"
+}
+output "name_prefix_skeleton_id" {
+  value = "${aws_security_group.name_prefix_skeleton_id.id}"
 }
