75ee72b0f5e5b80938c70bc7d8e2a602aa9edfb6
Aghogho A Omoware <a.omoware@comicrelief.com>
2018-01-10 10:26:59 +0000

    IPv6 support

--- aws/security_group_rule/inputs.tf
+++ aws/security_group_rule/inputs.tf
@@ -6,5 +6,6 @@ variable from_port                { default = "" }
 variable description              { default = "" }
 variable cidr_blocks              { default = [] type = "list" }
 variable prefix_list_ids          { default = [] type = "list" }
+variable ipv6_cidr_blocks         { default = [] type = "list" }
 variable security_group_id        { default = "" }
 variable source_security_group_id { default = "" }
--- aws/security_group_rule/security_group_rule.tf
+++ aws/security_group_rule/security_group_rule.tf
@@ -8,6 +8,7 @@ resource "aws_security_group_rule" "rule" {
   cidr_blocks     = ["${var.cidr_blocks}"]
   prefix_list_ids = ["${var.prefix_list_ids}"]
 
+  ipv6_cidr_blocks = "${var.ipv6_cidr_blocks}"
   security_group_id = "${var.security_group_id}"
   source_security_group_id = "${var.source_security_group_id}"
 }
