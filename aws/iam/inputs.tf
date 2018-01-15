237decdb2bbc9529c74fc4259e4c6521f52c6ff3
Aghogho A Omoware <a.omoware@comicrelief.com>
2018-01-15 11:43:38 +0000

    split iam: iam_user

--- aws/iam/inputs.tf
+++ aws/iam/inputs.tf
@@ -8,16 +8,3 @@ variable "iam_role_force_detach_policies"    { default = true }
 variable "iam_role_policy_name"              { default = [] type = "list" }
 variable "iam_role_policy_role"              { default = {} type = "map" }
 variable "iam_role_policy_policy"            { default = {} type = "map" }
-
-
-
-#-- users
-
-variable "iam_user_names"                    { default = [] type = "list" }
-variable "iam_user_access_key"               { default = "true" }
-variable "iam_user_gpg_keybase"              { default = {} type = "map" }
-variable "iam_user_gpg_or_keybase"           { default = "" }
-variable "iam_user_password_length"          { default = "11" }
-variable "iam_user_group_membership_name"    { default = "" }
-variable "iam_user_group_membership_group"   { default = [] type = "list"}
-variable "iam_user_password_reset_required"  { default = true }
