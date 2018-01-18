12a47ef68c98dfd7916314df94c0a430ebe127f0
Aghogho A Omoware <a.omoware@comicrelief.com>
2018-01-18 16:20:03 +0000

    Message Broker

---
+++ aws/mq/mq_broker/mq_broker.tf
@@ -0,0 +1,27 @@
+resource "aws_mq_broker" "mb" {
+  broker_name = "${var.broker_name}"
+
+  configuration {
+    id       = "${var.id}"
+    revision = "${var.revision}"
+  }
+
+  subnet_ids         = ["${var.subnet_ids}"]
+  engine_type        = "${var.engine_type}"
+  engine_version     = "${var.engine_version}"
+  security_groups    = ["${var.security_groups}"]
+  host_instance_type = "${var.host_instance_type}"
+
+  user {
+    groups         = ["${var.groups}"]
+    username       = "${var.username}"
+    password       = "${var.password}"
+    console_access = "${var.console_access}"
+  }
+
+  maintenance_window_start_time {
+    time_zone    = "${var.time_zone}"
+    time_of_day  = "${var.time_of_day}"
+    day_of_week  = "${var.day_of_week}"
+  }
+}
