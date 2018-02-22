f9a746d749c7953600e394fcdbb2b26c906fe234
Aghogho A Omoware <a.omoware@comicrelief.com>
2018-02-22 17:29:06 +0000

    delegation_set_id conflicts with vpc_id

--- aws/route53/route53_zone/inputs.tf
+++ aws/route53/route53_zone/inputs.tf
@@ -4,3 +4,6 @@ variable comment            { default = "" }
 variable vpc_region         { default = "" }
 variable force_destroy      { default = "" }
 variable delegation_set_id  { default = "" }
+
+variable vpc                { default = false }
+variable set_id             { default = false }
--- aws/route53/route53_zone/route53_zone.tf
+++ aws/route53/route53_zone/route53_zone.tf
@@ -1,6 +1,17 @@
 resource "aws_route53_zone" "zones" {
+  count             = "${var.vpc ? 1 : 0}"
+
   name              = "${var.name}"
   vpc_id            = "${var.vpc_id}"
+  comment           = "${var.comment}"
+  vpc_region        = "${var.vpc_region}"
+  force_destroy     = "${var.force_destroy}"
+}
+
+resource "aws_route53_zone" "zones" {
+  count             = "${var.set_id ? 1 : 0}"
+
+  name              = "${var.name}"
   comment           = "${var.comment}"
   vpc_region        = "${var.vpc_region}"
   force_destroy     = "${var.force_destroy}"
