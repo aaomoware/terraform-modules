c1f32f6e6ef0574fe927901002be7af7f06c250e
Aghogho A Omoware <a.omoware@comicrelief.com>
2018-04-20 15:57:08 +0100

    indent

--- aws/route53/route53_record/output.tf
+++ aws/route53/route53_record/output.tf
@@ -6,12 +6,12 @@ output "simple_fqdn" {
   value = "${aws_route53_zone.simple.fqdn}"
 }
 
-output "weighted_fqdn" {
-  value = "${aws_route53_zone.weighted.fqdn}"
-}
-
 output "lentacy_fqdn" {
   value = "${aws_route53_zone.lentacy.fqdn}"
+}
+
+output "weighted_fqdn" {
+  value = "${aws_route53_zone.weighted.fqdn}"
 }
 
 output "failover_fqdn" {
