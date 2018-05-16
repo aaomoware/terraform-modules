3152d438a44a2c9312e7a0c4f5c17227bce48b45
Aghogho A Omoware <a_omoware@hotmail.com>
2018-05-16 14:04:54 +0100

    because of the to count conditional

--- aws/route53/route53_record/output.tf
+++ aws/route53/route53_record/output.tf
@@ -1,31 +1,31 @@
 output "alias_fqdn" {
-  value = "${aws_route53_record.alias.fqdn}"
+  value = "${aws_route53_record.alias.*.fqdn}"
 }
 
 output "simple_fqdn" {
-  value = "${aws_route53_record.simple.fqdn}"
+  value = "${aws_route53_record.simple.*.fqdn}"
 }
 
 output "lentacy_fqdn" {
-  value = "${aws_route53_record.lentacy.fqdn}"
+  value = "${aws_route53_record.lentacy.*.fqdn}"
 }
 
 output "weighted_fqdn" {
-  value = "${aws_route53_record.weighted.fqdn}"
+  value = "${aws_route53_record.weighted.*.fqdn}"
 }
 
 output "failover_fqdn" {
-  value = "${aws_route53_record.failover.fqdn}"
+  value = "${aws_route53_record.failover.*.fqdn}"
 }
 
 output "multivalue_fqdn" {
-  value = "${aws_route53_record.multivalue.fqdn}"
+  value = "${aws_route53_record.multivalue.*.fqdn}"
 }
 
 output "geo_country_fqdn" {
-  value = "${aws_route53_record.geo_country.fqdn}"
+  value = "${aws_route53_record.geo_country.*.fqdn}"
 }
 
 output "geo_continent_fqdn" {
-  value = "${aws_route53_record.geo_continent.fqdn}"
+  value = "${aws_route53_record.geo_continent.*.fqdn}"
 }
