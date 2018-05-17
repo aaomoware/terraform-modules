6e587040afd769d39ed21cbd7f25524aab7fc47e
Aghogho A Omoware <a_omoware@hotmail.com>
2018-05-17 14:27:55 +0100

    computational values fails - revert

--- aws/vpc/route_table_association/route_table_association.tf
+++ aws/vpc/route_table_association/route_table_association.tf
@@ -1,5 +1,5 @@
 resource "aws_route_table_association" "rt_assoc" {
-  count          = "${var.route_table_id}"
+  count          = "${length(var.route_table_id)}"
   subnet_id      = "${var.subnet_id}"
   route_table_id = "${element(var.route_table_id, count.index)}"
 }
