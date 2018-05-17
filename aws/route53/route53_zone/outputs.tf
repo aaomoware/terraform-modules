4f7c66b80d893634eaf43bd686505a7cbeef1101
Aghogho A Omoware <a_omoware@hotmail.com>
2018-05-17 10:54:55 +0100

    there is only 1 element

--- aws/route53/route53_zone/outputs.tf
+++ aws/route53/route53_zone/outputs.tf
@@ -1,8 +1,8 @@
 output "vpc_zone_id" {
-  value = "${element(concat(aws_route53_zone.zones_vpc.*.zone_id, list("")), 0)}"
+  value = "${element(aws_route53_zone.zones_vpc.*.zone_id,0)}"
 }
 output "vpc_name_servers" {
-  value = "${element(concat(aws_route53_zone.zones_vpc.*.name_servers, list("")), 0)}"
+  value = "${element(aws_route53_zone.zones_vpc.*.name_servers,0)}"
 }
 
 
