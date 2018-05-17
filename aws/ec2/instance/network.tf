074897421a4a4996d887cbc90a6daf9f7ae6a435
Aghogho A Omoware <a_omoware@hotmail.com>
2018-05-17 16:26:23 +0100

    separate implementations

--- aws/ec2/instance/network.tf
+++ aws/ec2/instance/network.tf
@@ -14,11 +14,9 @@ resource "aws_instance" "network" {
   ipv6_addresses                       = "${var.ipv6_addresses}"
   security_groups                      = ["${var.security_groups}"]
   placement_group                      = "${var.placement_group}"
-  user_data_base64                     = "${var.user_data_base64}"
   source_dest_check                    = "${var.source_dest_check}"
   get_password_data                    = "${var.get_password_data}"
   availability_zone                    = "${var.availability_zone}"
-  ipv6_address_count                   = "${var.ipv6_address_count}"
   iam_instance_profile                 = "${var.iam_instance_profile}"
   vpc_security_group_ids               = ["${var.vpc_security_group_ids}"]
   disable_api_termination              = "${var.disable_api_termination}"
@@ -29,23 +27,6 @@ resource "aws_instance" "network" {
     create = "${var.create}"
     update = "${var.update}"
     delete = "${var.delete}"
-  }
-
-  root_block_device {
-    iops                  = "${var.root_iops}"
-    volume_type           = "${var.root_volume_type}"
-    volume_size           = "${var.root_volume_size}"
-    delete_on_termination = "${var.root_delete_on_termination}"
-  }
-
-  ebs_block_device {
-    iops                  = "${var.ebs_iops}"
-    encrypted             = "${var.encrypted}"
-    device_name           = "${var.ebs_device_name}"
-    snapshot_id           = "${var.snapshot_id}"
-    volume_type           = "${var.ebs_volume_type}"
-    volume_size           = "${var.ebs_volume_size}"
-    delete_on_termination = "${var.ebs_delete_on_termination}"
   }
 
   ephemeral_block_device {
