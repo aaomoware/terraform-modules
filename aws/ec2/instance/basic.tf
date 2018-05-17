691c8c70c034e313eccbc02c5bc2d3591608a9aa
Aghogho A Omoware <a_omoware@hotmail.com>
2018-05-17 13:50:44 +0100

    instance

---
+++ aws/ec2/instance/basic.tf
@@ -0,0 +1,62 @@
+resource "aws_instace" "basic" {
+  count         = "${var.basic ? 1: 0}"
+
+  ami                                  = "${var.ami}"
+  tenancy                              = "${var.tenancy}"
+  key_name                             = "${var.key_name}"
+  subnet_id                            = "${var.subnet_id}"
+  user_data                            = "${var.user_data}"
+  monitoring                           = "${var.monitoring}"
+  private_ip                           = "${var.private_ip}"
+  volume_tags                          = "${var.volume_tags}"
+  instance_type                        = "${var.instance_type}"
+  ebs_optimized                        = "${var.ebs_optimized}"
+  ipv6_addresses                       = "${var.ipv6_address}"
+  security_groups                      = ["${var.security_groups}"]
+  placement_group                      = "${var.placement_group}"
+  user_data_base64                     = "${var.user_data_base64}"
+  source_dest_check                    = "${var.source_dest_check}"
+  get_password_data                    = "${var.get+get_password_data}"
+  availability_zone                    = "${var.availability_zone}"
+  ipv6_address_count                   = "${var.ipv6_addrress_count}"
+  iam_instance_profile                 = "${var.iam_instance_profile}"
+  vpc_security_group_ids               = ["${var.vpc_security_group_ids}"]
+  disable_api_termination              = "${var.disable_api_termination}"
+  associate_public_ip_address          = "${var.associate_public_ip_address}"
+  instance_initiated_shutdown_behavior = "${var.instance_initiated_shutdown_behavior}"
+
+  timeouts {
+    create = "${var.create}"
+    update = "${var.updare}"
+    delete = "${var.delete}"
+  }
+
+root_block_device {
+   iops                  = "${var.root_iops}"
+   volume_type           = "${var.root_volume_type}"
+   volume_size           = "${var.root_volume_size}"
+   delete_on_termination = "${var.root_delete_on_termination}" }
+}
+
+ebs_block_device {
+   iops                  = "${var.ebs_iops}"
+   encrypted             = "${var.encrypted}"
+   device_name           = "${var.ebs_devce_name}"
+   snapshot_id           = "${var.snapshot_id}"
+   volume_type           = "${var.ebs_volume_type}"
+   volume_size           = "${var.ebs_volume_size}"
+   delete_on_termination = "${var.ebs_delete_on_termination}"
+}
+
+ephemeral_block_device {
+   no_device             = "${var.no_device}"
+   device_name           = "${var.ephemeral_device_name}"
+   virtual_name          = "${var.virtual_name}"
+}
+
+credit_specification {
+  cpu_credits = "${var.cpu_credits}"
+}
+
+  tags = "${var.tags}"
+}
---
+++ aws/ec2/instance/outputs.tf
@@ -0,0 +1,100 @@
+# basic
+output "basic_instance_id" {
+  value = "${element(concat(aws_instance.basic.*.id, list("")), 0)}"
+}
+output "basic_instance_availability_zone" {
+  value = "${element(concat(aws_instance.basic.*.availability_zone, list("")), 0)}"
+}
+output "basic_instance_placement_group" {
+  value = "${element(concat(aws_instance.basic.*.placement_group, list("")), 0)}"
+}
+output "basic_instance_key_name" {
+  value = "${element(concat(aws_instance.basic.*.key_name, list("")), 0)}"
+}
+output "basic_instance_password_data" {
+  value = "${element(concat(aws_instance.basic.*.password_data, list("")), 0)}"
+}
+output "basic_instance_public_dns" {
+  value = "${element(concat(aws_instance.basic.*.public_dns, list("")), 0)}"
+}
+output "basic_instance_public_ip" {
+  value = "${element(concat(aws_instance.basic.*.public_id, list("")), 0)}"
+}
+output "basic_instance_ipv6_addresses" {
+  value = "${aws_instance.basic.*.ipv6_addresses}"
+}
+output "basic_instance_network_interface_id" {
+  value = "${element(concat(aws_instance.basic.*.network_id, list("")), 0)}"
+}
+output "basic_instance_primary_network_interface_id" {
+  value = "${element(concat(aws_instance.basic.*.network_interface_id, list("")), 0)}"
+}
+output "basic_instance_private_dns" {
+  value = "${element(concat(aws_instance.basic.*.private_dns, list("")), 0)}"
+}
+output "basic_instance_private_ip" {
+  value = "${element(concat(aws_instance.basic.*.private_id, list("")), 0)}"
+}
+output "basic_instance_security_groups" {
+  value = "${element(concat(aws_instance.basic.*.security_groups, list("")), 0)}"
+}
+output "basic_instance_vpc_security_group_ids" {
+  value = "${element(concat(aws_instance.basic.*.vpc_security_group_ids, list("")), 0)}"
+}
+output "basic_instance_subnet_id" {
+  value = "${element(concat(aws_instance.basic.*.subnet_id, list("")), 0)}"
+}
+output "basic_instance_credit_specification" {
+  value = "${element(concat(aws_instance.basic.*.credit_specification, list("")), 0)}"
+}
+
+
+# network
+output "network_instance_id" {
+  value = "${element(concat(aws_instance.network.*.id, list("")), 0)}"
+}
+output "network_instance_availability_zone" {
+  value = "${element(concat(aws_instance.network.*.availability_zone, list("")), 0)}"
+}
+output "network_instance_placement_group" {
+  value = "${element(concat(aws_instance.network.*.placement_group, list("")), 0)}"
+}
+output "network_instance_key_name" {
+  value = "${element(concat(aws_instance.network.*.key_name, list("")), 0)}"
+}
+output "network_instance_password_data" {
+  value = "${element(concat(aws_instance.network.*.password_data, list("")), 0)}"
+}
+output "network_instance_public_dns" {
+  value = "${element(concat(aws_instance.network.*.public_dns, list("")), 0)}"
+}
+output "network_instance_public_ip" {
+  value = "${element(concat(aws_instance.network.*.public_id, list("")), 0)}"
+}
+output "network_instance_ipv6_addresses" {
+  value = "${aws_instance.network.*.ipv6_addresses}"
+}
+output "network_instance_network_interface_id" {
+  value = "${element(concat(aws_instance.network.*.network_id, list("")), 0)}"
+}
+output "network_instance_primary_network_interface_id" {
+  value = "${element(concat(aws_instance.network.*.network_interface_id, list("")), 0)}"
+}
+output "network_instance_private_dns" {
+  value = "${element(concat(aws_instance.network.*.private_dns, list("")), 0)}"
+}
+output "network_instance_private_ip" {
+  value = "${element(concat(aws_instance.network.*.private_id, list("")), 0)}"
+}
+output "network_instance_security_groups" {
+  value = "${element(concat(aws_instance.network.*.security_groups, list("")), 0)}"
+}
+output "network_instance_vpc_security_group_ids" {
+  value = "${element(concat(aws_instance.network.*.vpc_security_group_ids, list("")), 0)}"
+}
+output "network_instance_subnet_id" {
+  value = "${element(concat(aws_instance.network.*.subnet_id, list("")), 0)}"
+}
+output "network_instance_credit_specification" {
+  value = "${element(concat(aws_instance.network.*.credit_specification, list("")), 0)}"
+}
