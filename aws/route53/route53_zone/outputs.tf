60366f29b25299652e932bdf5e326b8a200f3e98
Aghogho A Omoware <a_omoware@hotmail.com>
2018-05-17 11:01:39 +0100

    testing

--- aws/route53/route53_zone/outputs.tf
+++ aws/route53/route53_zone/outputs.tf
@@ -1,9 +1,9 @@
 output "vpc_zone_id" {
   value = "${element(concat(aws_route53_zone.zones_vpc.*.zone_id, list("")), 0)}"
 }
-output "vpc_name_servers" {
-  value = "${element(concat(aws_route53_zone.zones_vpc.*.name_servers, list("")), 0)}"
-}
+# output "vpc_name_servers" {
+  # value = "${element(concat(aws_route53_zone.zones_vpc.*.name_servers, list("")), 0)}"
+# }
 
 
 output "set_id_zone_id" {
