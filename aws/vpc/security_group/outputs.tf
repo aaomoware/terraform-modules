a40cb935c42a921234d74c34df45b04cd3f492c9
Aghogho A Omoware <a_omoware@hotmail.com>
2018-05-17 13:59:18 +0100

    typo

--- aws/vpc/security_group/outputs.tf
+++ aws/vpc/security_group/outputs.tf
@@ -6,8 +6,8 @@ output "name_prefix_sg_id" {
 }
 
 output "name_skeleton_id" {
-  value = "${element(concat(aws_security_group.name_skeleton_id.*.id, list("")), 0)}"
+  value = "${element(concat(aws_security_group.name_skeleton.*.id, list("")), 0)}"
 }
 output "name_prefix_skeleton_id" {
-  value = "${element(concat(aws_security_group.name_prefix_skeleton_id.*.id, list("")), 0)}"
+  value = "${element(concat(aws_security_group.name_prefix_skeleton.*.id, list("")), 0)}"
 }
