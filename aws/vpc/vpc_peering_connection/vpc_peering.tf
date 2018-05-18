75d10de38f3e79f98109b0b217c1329c219d597b
Aghogho A Omoware <a_omoware@hotmail.com>
2018-05-18 11:31:34 +0100

    debugging

--- aws/vpc/vpc_peering_connection/vpc_peering.tf
+++ aws/vpc/vpc_peering_connection/vpc_peering.tf
@@ -4,13 +4,13 @@ resource "aws_vpc_peering_connection" "vpc_peering_conn" {
   peer_vpc_id   = "${var.peer_vpc_id}"
   peer_owner_id = "${var.peer_owner_id}"
 
-  accepter {
-    allow_remote_vpc_dns_resolution = "${var.accepter_requester["accepter"]}"
-  }
-
-  requester {
-    allow_remote_vpc_dns_resolution = "${var.accepter_requester["requester"]}"
-  }
+  # accepter {
+  #   allow_remote_vpc_dns_resolution = "${var.accepter_requester["accepter"]}"
+  # }
+  #
+  # requester {
+  #   allow_remote_vpc_dns_resolution = "${var.accepter_requester["requester"]}"
+  # }
 
   tags = "${var.tags}"
 
