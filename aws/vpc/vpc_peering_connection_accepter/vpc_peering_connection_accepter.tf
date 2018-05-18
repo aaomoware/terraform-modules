8f37c9cd95fcd6cf4093a473dc3e0c9e0954257e
Aghogho A Omoware <a_omoware@hotmail.com>
2018-05-18 13:47:54 +0100

    root level provider - revert

--- aws/vpc/vpc_peering_connection_accepter/vpc_peering_connection_accepter.tf
+++ aws/vpc/vpc_peering_connection_accepter/vpc_peering_connection_accepter.tf
@@ -1,5 +1,4 @@
 # Cross-Account VPC Peering Connection
-provider "aws" {}
 resource "aws_vpc_peering_connection_accepter" "vpc_p_con_acptr" {
   auto_accept               = "${var.auto_accept}"
   vpc_peering_connection_id = "${var.vpc_peering_connection_id}"
