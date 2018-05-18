e7959207b33501225c19a5913512424a888f3c32
Aghogho A Omoware <a_omoware@hotmail.com>
2018-05-18 10:11:40 +0100

    somewhat redundant

--- aws/vpc/vpc_peering_connection_accepter/inputs.tf
+++ aws/vpc/vpc_peering_connection_accepter/inputs.tf
@@ -1,5 +1,3 @@
 variable tags                      { default = {} type = "map" }
 variable auto_accept               { default = false }
-variable accept_status             { default = "" }
-variable provider_account          { default = "" }
 variable vpc_peering_connection_id { default = "" }
