61c1af64c88d872befc8b734e4e20e3bf61716ab
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-01 15:01:43 +0100

    enable auto rotate

--- aws/kms/kms_key/kms_key.tf
+++ aws/kms/kms_key/kms_key.tf
@@ -1,10 +1,10 @@
 resource "aws_kms_key" "kk" {
 
-  env                     = "${var.env}"
   policy                  = "${var.policy}"
   key_usage               = "${var.key_usage}"
   is_enabled              = "${var.is_enabled}"
   description             = "${var.description}"
+  enable_key_rotation     = "${var.enable_key_rotation}"
   deletion_window_in_days = "${var.deletion_window_in_days}"
 
   tags = "${var.tags}"
