740e3da6f9e8368a0070d55a6e7a5c4b4e46c78d
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-04 14:55:15 +0100

    redundant vars

--- aws/dynamodb/table/inputs.tf
+++ aws/dynamodb/table/inputs.tf
@@ -5,12 +5,7 @@ variable tags                     { default = {} type = "map" }
 variable attributes               { default = [] type = "list" }
 
 variable range_key                { default = "" }
-variable attribute_name           { default = "" }
 variable stream_view_type         { default = "" }
-variable local_secondary_index    { default = "" }
-variable global_secondary_index   { default = "" }
-variable server_side_encryption   { default = "" }
-variable point_in_time_recovery   { default = "" }
 
 variable read_capacity            { default = 20 }
 variable write_capacity           { default = 20 }
