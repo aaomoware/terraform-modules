5633d00dbbc04a095d62b4fc410193eabd4bf354
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-08 13:52:29 +0100

    duration format

--- aws/rds/db_option_group/inputs.tf
+++ aws/rds/db_option_group/inputs.tf
@@ -5,7 +5,7 @@ variable major_engine_version           {}
 variable tags                           { default = {} type = "map" }
 variable name                           { default = "" }
 variable port                           { default = "" }
-variable delete                         { default = 15 }
+variable delete                         { default = "15m" }
 variable version                        { default = "" }
 variable name_prefix                    { default = "" }
 variable option_group_description       { default = "Managed by Terraform" }
