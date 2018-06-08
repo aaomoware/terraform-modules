842249b6c83acff1da92124fa56c55fc59000215
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-08 13:44:15 +0100

    options

--- aws/rds/db_option_group/db_option_group.tf
+++ aws/rds/db_option_group/db_option_group.tf
@@ -1,7 +1,7 @@
 resource "aws_db_option_group" "dog" {
   tags                     = "${var.tags}"
   name                     = "${var.name}"
-  option                   = "${var.option}"
+  option                   = "${var.options}"
   engine_name              = "${var.engine_name}"
   major_engine_version     = "${var.major_engine_version}"
   option_group_description = "${var.option_group_description}"
