940800e4ee665991a3a0d969d85ac56a23e6eac0
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-12 13:05:55 +0100

    var.path

--- aws/iam/iam_user/iam_user.tf
+++ aws/iam/iam_user/iam_user.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_user" "iu" {
   count         = "${length(var.name) != 0 ? length(var.name) : 0}"
   name          = "${element(var.name,count.index)}"
-  path          = "${path[element(var.name,count.index)]}"
+  path          = "${var.path[element(var.name,count.index)]}"
   force_destroy = "${var.force_destroy}"
 }
