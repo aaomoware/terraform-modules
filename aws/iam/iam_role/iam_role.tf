d2e669516f578894f0197f33adeeb9e0ff8c7fb9
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-12 17:14:06 +0100

    typo

--- aws/iam/iam_role/iam_role.tf
+++ aws/iam/iam_role/iam_role.tf
@@ -1,9 +1,9 @@
 resource "aws_iam_role" "ir" {
-  count                 = "${length(var.names) != 0 ? length(var.names) : 0}"
-  name                  = "${element(var.names,count.index)}"
+  count                 = "${length(var.name) != 0 ? length(var.name) : 0}"
+  name                  = "${element(var.name,count.index)}"
   path                  = "${var.paths}"
-  description           = "${var.descriptiosn[element(var.names,count.index)]}"
-  assume_role_policy    = "${var.assume_role_policy[element(var.names,count.index)]}"
+  description           = "${var.descriptiosn[element(var.name,count.index)]}"
+  assume_role_policy    = "${var.assume_role_policy[element(var.name,count.index)]}"
   max_session_duration  = "${var.max_session_duration}"
   force_detach_policies = "${var.force_detach_policies}"
 }
