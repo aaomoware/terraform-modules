218f9fe3ab1e950fc6876c9d80de0c0a74a9dfe0
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-12 17:12:34 +0100

    typo

--- aws/iam/iam_role/outputs.tf
+++ aws/iam/iam_role/outputs.tf
@@ -1,11 +1,11 @@
 output "arn" {
   value = "${element(concat(aws_iam_role.ir.*.arn,list("")),0)}"
 }
+output "name" {
+  value = "${element(concat(aws_iam_role.ir.*.name,list("")),0)}"
+}
 output "unique_id" {
   value = "${element(concat(aws_iam_role.ir.*.unique_id,list("")),0)}"
-}
-output "name" {
-  value = "${element(concat(aws_iam_role.ir.*.name = "",list("")),0)}"
 }
 output "create_date" {
   value = "${element(concat(aws_iam_role.ir.*.create_date,list("")),0)}"
