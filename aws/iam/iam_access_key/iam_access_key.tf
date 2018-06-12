4312e7c7eda2d76b6d1e8061b94cfa6f8eb78c49
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-12 13:11:41 +0100

    maps

--- aws/iam/iam_access_key/iam_access_key.tf
+++ aws/iam/iam_access_key/iam_access_key.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_access_key" "ak" {
-  count   = "${length(var.user == length(var.gpg_key) ? 1: 0)}"
+  count   = "${length(var.user == length(values(var.gpg_key)) ? 1: 0)}"
   user    = "${element(var.user, count.index)}"
-  gpg_key = "${element(var.gpg_key, count.index)}"
+  gpg_key = "${var.gpg_key[element(var.user,count.index)])}"
 }
