37f02d515c355e3f27353804a6420f46ad7e27ed
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-12 17:17:20 +0100

    in seconds

--- aws/iam/iam_role/inputs.tf
+++ aws/iam/iam_role/inputs.tf
@@ -2,5 +2,5 @@ variable name                  { default = [] type = "list" }
 variable path                  { default = "/" }
 variable description           { default = {} type = "map" }
 variable assume_role_policy    { default = {} type = "map" }
-variable max_session_duration  { default = 12 }
+variable max_session_duration  { default = 43200 }
 variable force_detach_policies { default = true }
