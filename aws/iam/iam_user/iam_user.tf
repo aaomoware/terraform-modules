f50d69b76a0400bf69d057550e584b09de1b2780
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-12 12:51:29 +0100

    proper names

--- aws/iam/iam_user/iam_user.tf
+++ aws/iam/iam_user/iam_user.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_user" "iu" {
-  count         = "${length(var.names) != 0 ? length(var.names) : 0}"
-  name          = "${element(var.names,count.index)}"
-  path          = "${path[element(var.names,count.index)]}"
+  count         = "${length(var.name) != 0 ? length(var.name) : 0}"
+  name          = "${element(var.name,count.index)}"
+  path          = "${path[element(var.name,count.index)]}"
   force_destroy = "${var.force_destroy}"
 }
--- aws/iam/iam_user/inputs.tf
+++ aws/iam/iam_user/inputs.tf
@@ -1,3 +1,3 @@
-variable names         { default = [] type = "list" }
-variable paths         { default = {} type = "map" }
+variable name          { default = [] type = "list" }
+variable path          { default = {} type = "map" }
 variable force_destroy { default = true }
