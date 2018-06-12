1864ef312c861ef66b71c8735b64d53894cdabec
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-12 13:37:59 +0100

    omit secrets

--- aws/iam/iam_access_key/outputs.tf
+++ aws/iam/iam_access_key/outputs.tf
@@ -10,9 +10,6 @@ output "ak_secret_key" {
 output "ak_key_fingerprint" {
   value = "${element(concat(aws_iam_access_key.ak.*.key_fingerprint,list("")),0)}"
 }
-output "ak_secret_encrypted_secret" {
-  value = "${element(concat(aws_iam_access_key.ak.*.encrypted_secret,list("")),0)}"
-}
 output "ak_secret_ses_smtp_password" {
   value = "${element(concat(aws_iam_access_key.ak.*.ses_smtp_password,list("")),0)}"
 }
