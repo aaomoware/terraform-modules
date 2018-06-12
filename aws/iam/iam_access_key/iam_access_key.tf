2c0a97f837f31f6cf499b6d401a3dcd5601ce63b
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-12 13:21:16 +0100

    for mow

--- aws/iam/iam_access_key/iam_access_key.tf
+++ aws/iam/iam_access_key/iam_access_key.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_access_key" "ak" {
-  count   = "${length(var.user == length(keys(var.pgp_key)) ? 1: 0)}"
+  count   = "${length(var.user)}"
   user    = "${element(var.user, count.index)}"
   pgp_key = "${var.pgp_key[element(var.user,count.index)]}"
 }
