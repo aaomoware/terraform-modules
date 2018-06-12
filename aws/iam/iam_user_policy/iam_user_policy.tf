7fa0fe97c7be4ffe830280eda5d895b3002beb0e
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-12 12:58:02 +0100

    undeclared vars

--- aws/iam/iam_user_policy/iam_user_policy.tf
+++ aws/iam/iam_user_policy/iam_user_policy.tf
@@ -1,13 +1,13 @@
 resource "aws_iam_user_policy" "iup" {
   count  = "${var.prefix ? 0 : 1}"
-  name   = "${var.iam_user_policy_name}"
-  user   = "${var.iam_user_policy_user}"
-  policy = "${var.iam_user_policy_policy}"
+  name   = "${var.name}"
+  user   = "${var.user}"
+  policy = "${var.policy}"
 }
 
 resource "aws_iam_user_policy" "iup_prefix" {
   count         = "${var.prefix ? 1 : 0}"
-  user          = "${var.iam_user_policy_user}"
-  policy        = "${var.iam_user_policy_policy}"
-  name_prefix   = "${var.iam_user_policy_name_prefix}"
+  user          = "${var.user}"
+  policy        = "${var.policy}"
+  name_prefix   = "${var.name_prefix}"
 }
