7bde7c8e289634dc55e3f2c9fbab4f28372f057d
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-13 09:28:53 +0100

    assume policy for each role

--- aws/iam/iam_role/iam_role.tf
+++ aws/iam/iam_role/iam_role.tf
@@ -3,7 +3,22 @@ resource "aws_iam_role" "ir" {
   name                  = "${element(var.name,count.index)}"
   path                  = "${var.path}"
   description           = "${var.description[element(var.name,count.index)]}"
-  assume_role_policy    = "${var.assume_role_policy[element(var.name,count.index)]}"
+  assume_role_policy    = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "${var.assume_role_policy[element(var.name,count.index)]}.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
   max_session_duration  = "${var.max_session_duration}"
   force_detach_policies = "${var.force_detach_policies}"
 }
