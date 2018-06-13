1f556858be13d82c0650b296ae2dec812eb8255e
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-13 16:35:02 +0100

    typo

--- aws/iam/iam_role_policy/outputs.tf
+++ aws/iam/iam_role_policy/outputs.tf
@@ -1,12 +1,12 @@
 output "id" {
-  value = "${element(concat(aws_iam_group_policy.igp.*.id,list("")),0)}"
+  value = "${element(concat(aws_iam_group_policy.irp.*.id,list("")),0)}"
 }
 output "group" {
-  value = "${element(concat(aws_iam_group_policy.igp.*.group,list("")),0)}"
+  value = "${element(concat(aws_iam_group_policy.irp.*.group,list("")),0)}"
 }
 output "name" {
-  value = "${element(concat(aws_iam_group_policy.igp.*.name,list("")),0)}"
+  value = "${element(concat(aws_iam_group_policy.irp.*.name,list("")),0)}"
 }
 output "policy" {
-  value = "${element(concat(aws_iam_group_policy.igp.*.policy,list("")),0)}"
+  value = "${element(concat(aws_iam_group_policy.irp.*.policy,list("")),0)}"
 }
