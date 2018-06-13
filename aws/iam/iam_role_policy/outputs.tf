59e085604e0d71b09735819cdf37c22901a67fe7
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-13 16:36:57 +0100

    iam_role_policy

--- aws/iam/iam_role_policy/outputs.tf
+++ aws/iam/iam_role_policy/outputs.tf
@@ -1,12 +1,12 @@
 output "id" {
-  value = "${element(concat(aws_iam_group_policy.irp.*.id,list("")),0)}"
+  value = "${element(concat(aws_iam_role_policy.irp.*.id,list("")),0)}"
 }
 output "group" {
-  value = "${element(concat(aws_iam_group_policy.irp.*.group,list("")),0)}"
+  value = "${element(concat(aws_iam_role_policy.irp.*.group,list("")),0)}"
 }
 output "name" {
-  value = "${element(concat(aws_iam_group_policy.irp.*.name,list("")),0)}"
+  value = "${element(concat(aws_iam_role_policy.irp.*.name,list("")),0)}"
 }
 output "policy" {
-  value = "${element(concat(aws_iam_group_policy.irp.*.policy,list("")),0)}"
+  value = "${element(concat(aws_iam_role_policy.irp.*.policy,list("")),0)}"
 }
