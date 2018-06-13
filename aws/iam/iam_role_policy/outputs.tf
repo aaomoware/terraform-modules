bee80be0037d6e6fa6a5302c699ac78f466ecdd5
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-13 16:40:52 +0100

    typo

--- aws/iam/iam_role_policy/outputs.tf
+++ aws/iam/iam_role_policy/outputs.tf
@@ -1,8 +1,8 @@
 output "id" {
   value = "${element(concat(aws_iam_role_policy.irp.*.id,list("")),0)}"
 }
-output "group" {
-  value = "${element(concat(aws_iam_role_policy.irp.*.group,list("")),0)}"
+output "role" {
+  value = "${element(concat(aws_iam_role_policy.irp.*.role,list("")),0)}"
 }
 output "name" {
   value = "${element(concat(aws_iam_role_policy.irp.*.name,list("")),0)}"
