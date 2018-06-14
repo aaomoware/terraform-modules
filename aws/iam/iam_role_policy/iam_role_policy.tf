bd810e2c9141e15fd283bd6787df1a1c46b4c11a
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-14 10:16:59 +0100

    use the role name as the policy name

--- aws/iam/iam_role_policy/iam_role_policy.tf
+++ aws/iam/iam_role_policy/iam_role_policy.tf
@@ -1,6 +1,6 @@
 resource "aws_iam_role_policy" "irp" {
   count  = "${length(var.names) != 0 ? length(var.names) : 0}"
   name   = "${element(var.names,count.index)}"
-  role   = "${var.roles[element(var.names,count.index)]}"
+  role   = "${element(var.names,count.index)}"
   policy = "${var.policies[element(var.names,count.index)]}"
 }
