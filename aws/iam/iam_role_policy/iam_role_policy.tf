a805f827020846d086698a7875ccb9c5197a6dd7
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-18 13:55:29 +0100

    trimspace

--- aws/iam/iam_role_policy/iam_role_policy.tf
+++ aws/iam/iam_role_policy/iam_role_policy.tf
@@ -2,5 +2,5 @@ resource "aws_iam_role_policy" "irp" {
   count  = "${length(var.names) != 0 ? length(var.names) : 0}"
   name   = "${element(var.names,count.index)}"
   role   = "${var.roles[element(var.names,count.index)]}"
-  policy = "${jsonencode(var.policies[element(var.names,count.index)])}"
+  policy = "${trimspace(var.policies[element(var.names,count.index)])}"
 }
