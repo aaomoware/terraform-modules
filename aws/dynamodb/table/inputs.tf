23eb6fc39074f06a00dfd85982b7ae9f690063f5
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-19 14:17:00 +0100

    aligned

--- aws/dynamodb/table/inputs.tf
+++ aws/dynamodb/table/inputs.tf
@@ -1,23 +1,23 @@
-variable name                     {}
-variable hash_key                 {}
+variable name                                       {}
+variable hash_key                                   {}
 
-variable tags                     { default = {} type = "map" }
-variable attributes               { default = [] type = "list" }
+variable tags                                       { default = {} type = "map" }
+variable attributes                                 { default = [] type = "list" }
 
-variable range_key                { default = "" }
-variable stream_view_type         { default = "" }
+variable range_key                                  { default = "" }
+variable stream_view_type                           { default = "" }
 
-variable read_capacity            { default = 20 }
-variable write_capacity           { default = 20 }
-variable ttl_attribute_name       { default = "TimeToExist" }
+variable read_capacity                              { default = 20 }
+variable write_capacity                             { default = 20 }
+variable ttl_attribute_name                         { default = "TimeToExist" }
 
-variable to_create                 { default = "10m" }
-variable to_update                 { default = "10m" }
-variable to_delete                 { default = "10m" }
-variable sse_enable                { default = false }
-variable pitr_enable               { default = false }
-variable ttl_enabled               { default = false }
-variable stream_enabled            { default = false }
+variable to_create                                  { default = "10m" }
+variable to_update                                  { default = "10m" }
+variable to_delete                                  { default = "10m" }
+variable sse_enable                                 { default = false }
+variable pitr_enable                                { default = false }
+variable ttl_enabled                                { default = false }
+variable stream_enabled                             { default = false }
 
 variable simple                                     { default = false }
 variable local_secondary_index                      { default = false }
