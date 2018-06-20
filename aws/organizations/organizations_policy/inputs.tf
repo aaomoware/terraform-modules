4d0eaeee0edd2b4006121998af30c33453c3b5bd
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-20 15:56:11 +0100

    list

--- aws/organizations/organizations_policy/inputs.tf
+++ aws/organizations/organizations_policy/inputs.tf
@@ -2,4 +2,4 @@ variable name         { type = "list" }
 variable content      { type = "list" }
 
 variable type         { default = "" }
-variable description  { default = "" type = "list" }
+variable description  { default = [] type = "list" }
