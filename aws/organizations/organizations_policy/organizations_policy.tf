b1018c09a40bba1ec1e86a4ecafa5f4a5d2638d4
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-20 15:20:59 +0100

    type isnt a list

--- aws/organizations/organizations_policy/organizations_policy.tf
+++ aws/organizations/organizations_policy/organizations_policy.tf
@@ -2,7 +2,7 @@ resource "aws_organizations_policy" "op" {
   count       = "${length(var.names)}"
 
   name        = "${element(var.name, count.index)}"
-  type        = "${element(var.type, count.index)}"
+  type        = "${var.type}"
   content     = "${element(var.content, count.index)}"
   description = "${element(var.description, count.index)}"
 }
