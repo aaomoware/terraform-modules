89675b2c06f18b962835114bae1a87817a3fd985
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-20 16:08:50 +0100

    SERVICE_CONTROL_POLICY

--- aws/organizations/organizations_policy/inputs.tf
+++ aws/organizations/organizations_policy/inputs.tf
@@ -1,5 +1,5 @@
 variable name         { type = "list" }
 variable content      { type = "list" }
 
-variable type         { default = "" }
+variable type         { default = "SERVICE_CONTROL_POLICY" }
 variable description  { default = [] type = "list" }
