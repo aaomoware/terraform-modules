f21114957b7ba05efd4ea59c71254852cc2f1daa
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-21 11:11:02 +0100

    spits error when ids = 0. can be resolved by additioning a conditional - revert

--- aws/organizations/organizations_policy/outputs.tf
+++ aws/organizations/organizations_policy/outputs.tf
@@ -4,3 +4,9 @@ output "id" {
 output "arn" {
   value = "${aws_organizations_policy.op.*.arn}"
 }
+output "map_ids" {
+  value = "${zipmap(var.name, aws_organizations_policy.op.*.id)}"
+}
+output "map_arns" {
+  value = "${zipmap(var.name, aws_organizations_policy.op.*.arn)}"
+}
