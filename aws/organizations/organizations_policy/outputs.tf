d226da967537de811b187c0bea3ff992f301f47d
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-21 11:28:52 +0100

    equal

--- aws/organizations/organizations_policy/outputs.tf
+++ aws/organizations/organizations_policy/outputs.tf
@@ -5,8 +5,8 @@ output "arn" {
   value = "${aws_organizations_policy.op.*.arn}"
 }
 output "map_ids" {
-  value = "${length(aws_organizations_policy.op.*.id) = 0 ? var.empty_map : zipmap(var.name, aws_organizations_policy.op.*.id)}"
+  value = "${length(aws_organizations_policy.op.*.id) == 0 ? var.empty_map : zipmap(var.name, aws_organizations_policy.op.*.id)}"
 }
 output "map_arns" {
-  value = "${length(aws_organizations_policy.op.*.arn) = 0 ? var.empty_map : zipmap(var.name, aws_organizations_policy.op.*.arn)}"
+  value = "${length(aws_organizations_policy.op.*.arn) == 0 ? var.empty_map : zipmap(var.name, aws_organizations_policy.op.*.arn)}"
 }
