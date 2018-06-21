9d201cb22409a4e826a9a79be382fb7a994e5ac0
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-21 11:07:08 +0100

    spits error when ids = 0. can be resolved by additioning a conditional

--- aws/organizations/organizations_policy/outputs.tf
+++ aws/organizations/organizations_policy/outputs.tf
@@ -4,9 +4,3 @@ output "id" {
 output "arn" {
   value = "${aws_organizations_policy.op.*.arn}"
 }
-output "map_ids" {
-  value = "${zipmap(var.name, aws_organizations_policy.op.*.id)}"
-}
-output "map_arns" {
-  value = "${zipmap(var.name, aws_organizations_policy.op.*.arn)}"
-}
