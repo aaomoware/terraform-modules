61e009c4e2461d84aa2b6746fa1159b4a66c5a55
Aghogho A Omoware <a_omoware@hotmail.com>
2018-06-21 11:27:45 +0100

    test if list is empty

--- aws/organizations/organizations_policy/inputs.tf
+++ aws/organizations/organizations_policy/inputs.tf
@@ -2,4 +2,5 @@ variable name         { type = "list" }
 variable content      { type = "list" }
 
 variable type         { default = "SERVICE_CONTROL_POLICY" }
+variable empty_map    { default = {} type = "map" }
 variable description  { default = [] type = "list" }
