e2a842d60811ecdf11e7bd28beea19fb8f2d5325
Aghogho A Omoware <a_omoware@hotmail.com>
2018-07-26 17:21:48 +0100

    associate in a swoop

--- aws/vpc/route_table_association/route_table_association.tf
+++ aws/vpc/route_table_association/route_table_association.tf
@@ -1,4 +1,6 @@
 resource "aws_route_table_association" "rt_assoc" {
-  subnet_id      = "${var.subnet_id}"
+  count = "${length(var.subnet_id)}"
+
+  subnet_id      = "${element(var.subnet_id, count.index)}"
   route_table_id = "${var.route_table_id}"
 }
