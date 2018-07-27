27a0cae850cf05263d423c229539223ca1225f40
Aghogho A Omoware <a_omoware@hotmail.com>
2018-07-27 14:33:35 +0100

    outputs for both resource

--- aws/vpc/security_group_rule/outputs.tf
+++ aws/vpc/security_group_rule/outputs.tf
@@ -1,3 +1,7 @@
-output rule_id {
-  value = "${element(concat(aws_security_group_rule.rule.*.id, list("")),0)}"
+output ssg_id {
+  value = "${element(concat(aws_security_group_rule.ss_group_id.*.id, list("")),0)}"
+}
+
+output cb_id {
+  value = "${element(concat(aws_security_group_rule.cidr_block.*.id, list("")),0)}"
 }
