f4c7c591afd0214028045b517ddb7d0437712476
Aghogho A Omoware <a_omoware@hotmail.com>
2018-07-30 10:46:15 +0100

    skeleton

--- aws/vpc/security_group/outputs.tf
+++ aws/vpc/security_group/outputs.tf
@@ -1,13 +1,3 @@
-output "name_sg_id" {
-  value = "${element(concat(aws_security_group.name.*.id, list("")), 0)}"
-}
-output "name_prefix_sg_id" {
-  value = "${element(concat(aws_security_group.name_prefix.*.id, list("")), 0)}"
-}
-
-output "name_skeleton_id" {
-  value = "${element(concat(aws_security_group.name_skeleton.*.id, list("")), 0)}"
-}
-output "name_prefix_skeleton_id" {
-  value = "${element(concat(aws_security_group.name_prefix_skeleton.*.id, list("")), 0)}"
+output "id" {
+  value = "${aws_security_group.sg.id}"
 }
--- aws/vpc/security_group/security_group.tf
+++ aws/vpc/security_group/security_group.tf
@@ -1,57 +1,7 @@
 #---name, fixed name
-resource "aws_security_group" "name" {
-  count = "${var.named ? 1 : 0}"
+resource "aws_security_group" "sg" {
 
   name        = "${var.name}"
-  vpc_id      = "${var.vpc_id}"
-  description = "${var.description}"
-  revoke_rules_on_delete = "${var.revoke_rules_on_delete}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = "${var.tags}"
-}
-
-#---name_prefix: creates unique name
-resource "aws_security_group" "name_prefix" {
-  count = "${var.name_prefix ? 1 : 0}"
-
-  name_prefix = "${var.name}"
-  vpc_id      = "${var.vpc_id}"
-  description = "${var.description}"
-  revoke_rules_on_delete = "${var.revoke_rules_on_delete}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = "${var.tags}"
-}
-
-
-#---skeleton security_group; name_skeleton
-resource "aws_security_group" "name_skeleton" {
-  count = "${var.name_skeleton ? 1 : 0}"
-
-  name        = "${var.name}"
-  vpc_id      = "${var.vpc_id}"
-  description = "${var.description}"
-  revoke_rules_on_delete = "${var.revoke_rules_on_delete}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = "${var.tags}"
-}
-
-#---skeleton security_group; name_prefix_skeleton
-resource "aws_security_group" "name_prefix_skeleton" {
-  count = "${var.name_prefix_skeleton ? 1 : 0}"
-
-  name_prefix = "${var.name}"
   vpc_id      = "${var.vpc_id}"
   description = "${var.description}"
   revoke_rules_on_delete = "${var.revoke_rules_on_delete}"
