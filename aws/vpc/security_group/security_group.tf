5126bf28c801157b20d75acede9767f1e1c9f055
Aghogho A Omoware <a_omoware@hotmail.com>
2018-07-30 11:08:29 +0100

    causing recursive loop?

--- aws/vpc/security_group/security_group.tf
+++ aws/vpc/security_group/security_group.tf
@@ -1,14 +1,9 @@
 #---name, fixed name
 resource "aws_security_group" "sg" {
 
+  tags        = "${var.tags}"
   name        = "${var.name}"
   vpc_id      = "${var.vpc_id}"
   description = "${var.description}"
   revoke_rules_on_delete = "${var.revoke_rules_on_delete}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tags = "${var.tags}"
 }
