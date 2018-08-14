a92fea6ec9a021cb1f10576e8991f4c6a71244f7
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-14 16:28:41 +0100

    more options

--- aws/vpc/vpc_peering_connection/inputs.tf
+++ aws/vpc/vpc_peering_connection/inputs.tf
@@ -1,12 +1,13 @@
 variable tags                { default = {} type = "map" }
 variable vpc_id              { default = "" }
-variable auto_accept         { default = true }
+variable peer_region         { default = "" }
+variable auto_accept         { default = false }
 variable peer_vpc_id         { default = "" }
 variable peer_owner_id       { default = "" }
 variable accepter_requester  {
   default = {
     accepter = false
     requester = false
-    }
+  }
   type = "map"
 }
--- aws/vpc/vpc_peering_connection/vpc_peering.tf
+++ aws/vpc/vpc_peering_connection/vpc_peering.tf
@@ -1,20 +1,16 @@
 resource "aws_vpc_peering_connection" "vpc_peering_conn" {
+  tags          = "${var.tags}"
   vpc_id        = "${var.vpc_id}"
   auto_accept   = "${var.auto_accept}"
+  peer_region   = "${var.peer_region}"
   peer_vpc_id   = "${var.peer_vpc_id}"
   peer_owner_id = "${var.peer_owner_id}"
 
-  # accepter {
-  #   allow_remote_vpc_dns_resolution = "${var.accepter_requester["accepter"]}"
-  # }
-  #
-  # requester {
-  #   allow_remote_vpc_dns_resolution = "${var.accepter_requester["requester"]}"
-  # }
-
-  tags = "${var.tags}"
-
-  lifecycle {
-    create_before_destroy = true
-  }
+   accepter {
+     allow_remote_vpc_dns_resolution = "${var.accepter_requester["accepter"]}"
+   }
+   requester {
+     allow_remote_vpc_dns_resolution = "${var.accepter_requester["requester"]}"
+   }
 }
+s
