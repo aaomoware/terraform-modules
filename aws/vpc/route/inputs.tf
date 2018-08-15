78dbcfc40c198fb62fc3f47531a529358a24ecf0
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-15 13:55:15 +0100

    internet/nat way list addition

--- aws/vpc/route/inputs.tf
+++ aws/vpc/route/inputs.tf
@@ -1,7 +1,8 @@
-variable gateway_id                { default = "" }
+variable default                   { default = true }
+variable gateway_id                { default = [] type = "list" }
 variable instance_id               { default = "" }
 variable route_table_id            {}
-variable nat_gateway_id            { default = "" }
+variable nat_gateway_id            { default = [] type = "list" }
 variable network_interface_id      { default = "" }
 variable egress_only_gateway_id    { default = "" }
 variable destination_cidr_block    { default = "" }
--- aws/vpc/route/route.tf
+++ aws/vpc/route/route.tf
@@ -1,14 +1,25 @@
-resource "aws_route" "route" {
-  gateway_id                = "${var.gateway_id}"
+#<---generic one-to-one
+resource "aws_route" "rt" {
+  count = "${var.default ? 1 : 0}"
+
+  gateway_id                = "${element(var.gateway_id, length(var.gateway_id))}"
   instance_id               = "${var.instance_id}"
   route_table_id            = "${var.route_table_id}"
-  nat_gateway_id            = "${var.nat_gateway_id}"
+  nat_gateway_id            = "${element(var.nat_gateway_id, length(var.nat_gateway_id)}"
   network_interface_id      = "${var.network_interface_id}"
   destination_cidr_block    = "${var.destination_cidr_block}"
   egress_only_gateway_id    = "${var.egress_only_gateway_id}"
   vpc_peering_connection_id = "${var.vpc_peering_connection_id}"
+}
 
-  lifecycle {
-    create_before_destroy = true
-  }
+# ----------
+# for lists of gateways and nat_gateway ids.
+# we can add these routes in a swoop, instead of calling this module for each gateway and nat_gateway route add
+#------------
+resource "aws_route" "rt_gn" {
+  count = "${!var.default && length(var.gateway_id) > 0 ? length(var.gateway_id) : length(var.nat_gateway_id)}"
+
+  gateway_id                = "${element(var.gateway_id, count.index)}"
+  route_table_id            = "${var.route_table_id}"
+  nat_gateway_id            = "${element(var.nat_gateway_id, count.index)}"
 }
