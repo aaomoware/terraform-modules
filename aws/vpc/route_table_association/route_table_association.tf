18aa157533a31bb7fce4f20cc43a0386d0829b0b
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-16 16:14:41 +0100

    it was right there in front of you!!

--- aws/vpc/route_table_association/route_table_association.tf
+++ aws/vpc/route_table_association/route_table_association.tf
@@ -1,5 +1,5 @@
 resource "aws_route_table_association" "rt_assoc" {
-  count = "${length(var.subnet_id, count.index)}"
+  count = "${length(var.subnet_id)}"
 
   subnet_id      = "${element(var.subnet_id, count.index)}"
   route_table_id = "${element(var.route_table_id, count.index)}"
