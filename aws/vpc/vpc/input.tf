842b2fc4636f9dc5c8cf6469b3061409e42fdae0
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-16 15:57:07 +0100

    enable/disable defaults as needed

--- aws/vpc/vpc/input.tf
+++ aws/vpc/vpc/input.tf
@@ -1,8 +1,6 @@
-variable env                              { default = "mars" }
 variable tags                             { default = {} type = "map" }
 variable cidr_block                       {}
 variable instance_tenancy                 { default = "default" }
-variable assign_generated_ipv6_cidr_block { default = false }
 
 variable basic_vpc                        { default = false }
 variable dns_support_vpc                  { default = false }
@@ -12,3 +10,4 @@ variable enable_dns_support               { default = true }
 variable enable_classiclink               { default = true }
 variable enable_dns_hostnames             { default = true }
 variable enable_classiclink_dns_support   { default = false }
+variable assign_generated_ipv6_cidr_block { default = false }
--- aws/vpc/vpc/vpc.tf
+++ aws/vpc/vpc/vpc.tf
@@ -1,12 +1,16 @@
 #--basic, vpc
 resource "aws_vpc" "basic_vpc" {
   count = "${var.basic_vpc ? 1 : 0}"
-  cidr_block = "${var.cidr_block}"
-
-  instance_tenancy = "${var.instance_tenancy}"
-  assign_generated_ipv6_cidr_block = "${var.assign_generated_ipv6_cidr_block}"
 
   tags = "${var.tags}"
+  cidr_block = "${var.cidr_block}"
+  instance_tenancy = "${var.instance_tenancy}"
+  enable_dns_support = "${var.enable_dns_support}"
+  enable_classiclink = "${var.enable_classiclink}"
+  enable_dns_hostnames = "${var.enable_dns_hostnames}"
+  enable_classiclink_dns_support = "${var.enable_classiclink_dns_support}"
+  assign_generated_ipv6_cidr_block = "${var.assign_generated_ipv6_cidr_block}"
+
 }
 
 
@@ -30,8 +34,6 @@ resource "aws_vpc" "classiclink_vpc" {
   count = "${var.classiclink_vpc ? 1 :0}"
 
   cidr_block = "${var.cidr_block}"
-  enable_classiclink = "${var.enable_classiclink}"
-  enable_classiclink_dns_support = "${var.enable_classiclink_dns_support}"
 
   instance_tenancy = "${var.instance_tenancy}"
   assign_generated_ipv6_cidr_block = "${var.assign_generated_ipv6_cidr_block}"
