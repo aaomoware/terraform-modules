ddf5d8fb0a9da4cdb44526d7f3a7a8ad744c4e27
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-16 17:34:36 +0100

    remove tags for now

--- aws/vpc/nat_gateway/inputs.tf
+++ aws/vpc/nat_gateway/inputs.tf
@@ -1,3 +1,2 @@
-variable tags          { default = [] type = "list" }
 variable subnet_id     { default = [] type = "list" }
 variable allocation_id { default = [] type = "list" }
--- aws/vpc/nat_gateway/nat_gateway.tf
+++ aws/vpc/nat_gateway/nat_gateway.tf
@@ -1,7 +1,6 @@
 resource "aws_nat_gateway" "nat_gtw" {
   count         = "${length(var.subnet_id)}"
 
-  tags          = "${element(var.tags, count.index)}"
   subnet_id     = "${element(var.subnet_id, count.index)}"
   allocation_id = "${element(var.allocation_id, count.index)}"
 }
