f44e62a699a79f40ff2969f77c74553490594760
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-17 13:46:13 +0100

    testing

--- aws/vpc/route_table_association/inputs.tf
+++ aws/vpc/route_table_association/inputs.tf
@@ -1,2 +1,3 @@
+variable rt_count       { default = "0" }
 variable subnet_id      { type = "list" }
 variable route_table_id { type = "list" }
--- aws/vpc/route_table_association/route_table_association.tf
+++ aws/vpc/route_table_association/route_table_association.tf
@@ -1,5 +1,5 @@
 resource "aws_route_table_association" "rt_assoc" {
-  count = "${length(var.route_table_id) > 0 ? length(var.route_table_id) : 0}"
+  count = "${var.rt_count}"
 
   subnet_id      = "${element(var.subnet_id, count.index)}"
   route_table_id = "${element(var.route_table_id, count.index)}"
