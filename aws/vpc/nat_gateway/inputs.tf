53ee8fab661a0cfdb31c50feb0444885e6c50e18
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-17 12:58:38 +0100

    to satisfy: https://github.com/hashicorp/terraform/issues/10857\#issuecomment-368059147

--- aws/vpc/nat_gateway/inputs.tf
+++ aws/vpc/nat_gateway/inputs.tf
@@ -1,2 +1,7 @@
+variable sub_count     { default = "0" }
 variable subnet_id     { default = [] type = "list" }
 variable allocation_id { default = [] type = "list" }
+
+
+# sub_count is here till the below restiction is rectified by terraform
+# https://github.com/hashicorp/terraform/issues/10857#issuecomment-368059147
--- aws/vpc/nat_gateway/nat_gateway.tf
+++ aws/vpc/nat_gateway/nat_gateway.tf
@@ -1,5 +1,5 @@
 resource "aws_nat_gateway" "nat_gtw" {
-  count         = "${length(var.subnet_id) > 0 ? length(var.subnet_id) : 0}"
+  count         = "${var.sub_count > 0 ? var.sub_count : 0}"
 
   subnet_id     = "${element(var.subnet_id, count.index)}"
   allocation_id = "${element(var.allocation_id, count.index)}"
