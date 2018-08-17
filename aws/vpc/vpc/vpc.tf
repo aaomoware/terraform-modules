c15c845d9f90682a85c46b996da21847f84ccecb
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-17 14:08:22 +0100

    classlink not needed

--- aws/vpc/vpc/vpc.tf
+++ aws/vpc/vpc/vpc.tf
@@ -6,9 +6,7 @@ resource "aws_vpc" "basic_vpc" {
   cidr_block = "${var.cidr_block}"
   instance_tenancy = "${var.instance_tenancy}"
   enable_dns_support = "${var.enable_dns_support}"
-  enable_classiclink = "${var.enable_classiclink}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
-  enable_classiclink_dns_support = "${var.enable_classiclink_dns_support}"
   assign_generated_ipv6_cidr_block = "${var.assign_generated_ipv6_cidr_block}"
 
 }
