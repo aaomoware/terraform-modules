e3c405654306fadc50b659dfa8d89f53da523f46
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-17 12:46:44 +0100

    testing

--- aws/vpc/nat_gateway/nat_gateway.tf
+++ aws/vpc/nat_gateway/nat_gateway.tf
@@ -1,5 +1,5 @@
 resource "aws_nat_gateway" "nat_gtw" {
-  count         = "${length(var.allocation_id)}"
+  count         = "${length(var.subnet_id) > 0 ? length(var.subnet_id) : 0}"
 
   subnet_id     = "${element(var.subnet_id, count.index)}"
   allocation_id = "${element(var.allocation_id, count.index)}"
