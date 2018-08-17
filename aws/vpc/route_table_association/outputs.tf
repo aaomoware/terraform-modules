3ce757f5a8bb56bc1fd813942c28bb6561f578f7
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-17 14:03:40 +0100

    just output list

--- aws/vpc/route_table_association/outputs.tf
+++ aws/vpc/route_table_association/outputs.tf
@@ -1,3 +1,3 @@
 output "rt_assoc_id" {
-  value = "${length(var.rt_count) > 1 ? aws_route_table_association.rt_assoc.*.id : element(concat(aws_route_table_association.rt_assoc.*.id,list("")),0)}"
+  value = "${aws_route_table_association.rt_assoc.*.id}"
 }
