0e52dd5ad3cb6f6e24d0a338576f067a46f80b1f
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-17 15:08:15 +0100

    ofcourse

--- aws/vpc/route_table/route_table.tf
+++ aws/vpc/route_table/route_table.tf
@@ -2,5 +2,5 @@ resource "aws_route_table" "rt_table" {
   count = "${var.rt_count }"
 
   vpc_id = "${var.vpc_id}"
-  tags = "${merge(var.tags, map("Name", "${var.rt_count > 1 ? "${lookup(var.tags, "Name")}-${element(split(",", "${var.append}"), count.index)}" : "${lookup(var.tags, "Name")}" }"))}"
+  tags = "${merge(var.tags, map("Name", "${var.rt_count > 1 ? "${lookup(var.tags, "Name")} ${element(split(",", "${var.append}"), count.index)}" : "${lookup(var.tags, "Name")}" }"))}"
 }
