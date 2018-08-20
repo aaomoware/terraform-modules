4f1a3cba125d3610768148d4b218f3580daba4ec
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-20 13:57:54 +0100

    forgotten: needed

--- aws/vpc/route/route.tf
+++ aws/vpc/route/route.tf
@@ -26,10 +26,12 @@ resource "aws_route" "igw" {
 
     gateway_id                = "${element(var.gateway_id, count.index)}"
     route_table_id            = "${element(var.route_table_id, count.index)}"
+    destination_cidr_block    = "${var.destination_cidr_block}"
 }
 resource "aws_route" "ngw" {
    count = "${var.igw_count}"
 
    nat_gateway_id            = "${element(var.nat_gateway_id, count.index)}"
    route_table_id            = "${element(var.route_table_id, count.index)}"
+   destination_cidr_block    = "${var.destination_cidr_block}"
 }
