1cf067ce6ac4a0bbe8b1330bf3b635f553d704df
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-20 16:55:09 +0100

    variable destination_cidr_block    { default = "" }

--- aws/vpc/route_igw/inputs.tf
+++ aws/vpc/route_igw/inputs.tf
@@ -1,3 +1,4 @@
 variable igw_count                 { default = "0" }
 variable gateway_id                { default = [] type = "list" }
 variable route_table_id            { default = [] type = "list" }
+variable destination_cidr_block    { default = "" }
--- aws/vpc/route_nat/inputs.tf
+++ aws/vpc/route_nat/inputs.tf
@@ -1,3 +1,4 @@
 variable nat_count                 { default = "0" }
 variable route_table_id            { default = [] type = "list" }
 variable nat_gateway_id            { default = [] type = "list" }
+variable destination_cidr_block    { default = "" }
