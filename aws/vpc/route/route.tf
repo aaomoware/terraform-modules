67887866d28f331a073f22fb8f87fe808d277546
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-20 16:21:46 +0100

    temp fix?

--- aws/vpc/route/route.tf
+++ aws/vpc/route/route.tf
@@ -28,10 +28,10 @@ resource "aws_route" "igw" {
     route_table_id            = "${element(var.route_table_id, count.index)}"
     destination_cidr_block    = "${var.destination_cidr_block}"
 }
-resource "aws_route" "ngw" {
-   count = "${var.igw_count > 0 ? var.igw_count : 0}"
-
-   nat_gateway_id            = "${element(var.nat_gateway_id, count.index)}"
-   route_table_id            = "${element(var.route_table_id, count.index)}"
-   destination_cidr_block    = "${var.destination_cidr_block}"
+# resource "aws_route" "ngw" {
+#    count = "${var.igw_count > 0 ? var.igw_count : 0}"
+#
+#    nat_gateway_id            = "${element(var.nat_gateway_id, count.index)}"
+#    route_table_id            = "${element(var.route_table_id, count.index)}"
+#    destination_cidr_block    = "${var.destination_cidr_block}"
 }
