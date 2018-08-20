f93995ba1868bc568c5a9c9e1a964540f376ed8c
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-20 13:44:46 +0100

    temp fix

--- aws/vpc/route/inputs.tf
+++ aws/vpc/route/inputs.tf
@@ -1,10 +1,10 @@
 variable default                   { default = false }
 variable nat_count                 { default = "0" }
 variable igw_count                 { default = "0" }
-variable gateway_id                { default = [] type = "" }
+variable gateway_id                { default = ["a","b","c"] type = "list" }
 variable instance_id               { default = "" }
 variable route_table_id            { type = "list" }
-variable nat_gateway_id            { default = [] type = "list" }
+variable nat_gateway_id            { default = ["a","b","c"] type = "list" }
 variable network_interface_id      { default = "" }
 variable egress_only_gateway_id    { default = "" }
 variable destination_cidr_block    { default = "" }
