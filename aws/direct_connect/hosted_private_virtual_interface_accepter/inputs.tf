788d383d87c3b7b7fbd5051bc086f2e83f0c434e
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-21 13:09:53 +0100

    "dx_gateway_id": conflicts with vpn_gateway_id

--- aws/direct_connect/hosted_private_virtual_interface_accepter/inputs.tf
+++ aws/direct_connect/hosted_private_virtual_interface_accepter/inputs.tf
@@ -1,5 +1,4 @@
-variable tags                 { default = "" }
-variable dx_gateway_id        { default = "" }
+variable tags                 { default = {} type = "map" }
 variable vpn_gateway_id       { default = "" }
 variable virtual_interface_id {}
 
