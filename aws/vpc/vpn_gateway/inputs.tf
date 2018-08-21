1ad46cf28dea2b77a6ca792d79231ee1bc2394ad
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-21 10:34:38 +0100

    we want default asn

--- aws/vpc/vpn_gateway/inputs.tf
+++ aws/vpc/vpn_gateway/inputs.tf
@@ -1,3 +1,2 @@
 variable tags              { default = {} type = "map" }
 variable vpc_id            { default = "" }
-variable amazon_side_asn   { default = "" }
--- aws/vpc/vpn_gateway/vpn_gateway.tf
+++ aws/vpc/vpn_gateway/vpn_gateway.tf
@@ -1,5 +1,4 @@
 resource "aws_vpn_gateway" "vpn_gtw" {
   tags = "${var.tags}"
-  vpc_id = "${var.vpc_id}"
-  amazon_side_asn = "${var.amazon_side_asn}"
+  vpc_id = "${var.vpc_id}"s
 }
