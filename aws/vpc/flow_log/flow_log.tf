2a74b6588727ed166cb336655c6cd5f8b83901dd
Aghogho A Omoware <a_omoware@hotmail.com>
2018-08-22 15:23:32 +0100

    update

--- aws/vpc/flow_log/flow_log.tf
+++ aws/vpc/flow_log/flow_log.tf
@@ -1,5 +1,7 @@
 resource "aws_flow_log" "flow_log" {
   vpc_id         = "${var.vpc_id}"
+  eni_id         = "${var.eni_id}"
+  subnet_id      = "${var.subnet_id}"
   traffic_type   = "${var.traffic_type}"
   iam_role_arn   = "${var.iam_role_arn}"
   log_group_name = "${var.log_group_name}"
