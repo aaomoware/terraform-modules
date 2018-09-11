ab5c897b5ad371a95a20efa0f9268e3af27ac41d
Aghogho A Omoware <a_omoware@hotmail.com>
2018-09-11 17:40:51 +0100

    try map

--- aws/ec2/elb/inputs.tf
+++ aws/ec2/elb/inputs.tf
@@ -2,10 +2,10 @@ variable name                        { default = "" }
 variable tags                        { default = {} type = "map" }
 variable subnets                     { default = [] type = "list" }
 variable internal                    { default = "" }
-variable listener                    { default = [] type = "list" }
+variable listener                    { default = [] type = "map" }
 variable instances                   { default = [] type = "list" }
-variable access_logs                 { default = [] type = "list" }
-variable health_check                { default = [] type = "list" }
+variable access_logs                 { default = {} type = "map" }
+variable health_check                { default = {} type = "map" }
 variable idle_timeout                { default = 400 }
 variable security_groups             { default = [] type = "list" }
 variable availability_zones          { default = [] type = "list" }
