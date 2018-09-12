16a45ecbefb9f3f45d3ce76a28741976d25f1215
Aghogho A Omoware <a_omoware@hotmail.com>
2018-09-12 14:52:53 +0100

    we need tt

--- aws/acm_certificate/certificate/outputs.tf
+++ aws/acm_certificate/certificate/outputs.tf
@@ -7,6 +7,12 @@ output "arn" {
 output "validation_emails" {
   value = "${aws_acm_certificate.ac.validation_emails}"
 }
-output "domain_validation_options" {
-  value = "${aws_acm_certificate.ac.domain_validation_options}"
+output "resource_record_name" {
+  value = "${aws_acm_certificate.ac.domain_validation_options.0.resource_record_name}"
+}
+output "resource_record_type" {
+  value = "${aws_acm_certificate.ac.domain_validation_options.0.resource_record_type}"
+}
+output "resource_record_value" {
+  value = "${aws_acm_certificate.ac.domain_validation_options.0.resource_record_value}"
 }
