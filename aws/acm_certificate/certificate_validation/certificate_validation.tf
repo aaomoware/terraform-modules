5227fe8a2f8f2808313b8102c012f975da76a48b
Aghogho A Omoware <a_omoware@hotmail.com>
2018-09-12 14:56:09 +0100

    its a list

--- aws/acm_certificate/certificate_validation/certificate_validation.tf
+++ aws/acm_certificate/certificate_validation/certificate_validation.tf
@@ -1,6 +1,6 @@
 resource "aws_acm_certificate_validation" "acv" {
   certificate_arn         = "${var.certificate_arn}"
-  validation_record_fqdns = "${var.validation_record_fqdns}"
+  validation_record_fqdns = ["${var.validation_record_fqdns}"]
 
   timeouts {
     create = "${var.create}"
