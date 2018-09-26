605f66ad4e898ee22da2786e59920f57967500c6
Aghogho A Omoware <a_omoware@hotmail.com>
2018-09-26 16:57:20 +0100

    launch_configuration

---
+++ aws/ec2/launch_configuration/inputs.tf
@@ -0,0 +1,8 @@
+variable name                 {}
+variable key_name             {}
+variable image_id             {}
+variable user_data            {}
+variable instance_type        {}
+variable security_groups      {}
+variable iam_instance_profile {}
+variable create_before_destroy { default = true }
---
+++ aws/ec2/launch_configuration/launch_configuration.tf
@@ -0,0 +1,14 @@
+resource "aws_launch_configuration" "lc" {
+
+  name                  = "${var.name}"
+  key_name              = "${var.key_name}"
+  image_id              = "${var.image_id}"
+  user_data             = "${var.user_data}"
+  instance_type         = "${var.instance_type}"
+  security_groups       = "${var.security_groups}"
+  iam_instance_profile  = "${var.iam_instance_profile}"
+
+  lifecycle {
+    create_before_destroy = "${var.create_before_destroy}"
+  }
+}
