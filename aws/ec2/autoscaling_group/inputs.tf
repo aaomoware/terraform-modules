35f9fdc1f6df80c44a8284fa1c874652784217be
Aghogho A Omoware <a_omoware@hotmail.com>
2018-09-28 10:20:03 +0100

    its a list

--- aws/ec2/autoscaling_group/inputs.tf
+++ aws/ec2/autoscaling_group/inputs.tf
@@ -10,7 +10,7 @@ variable placement_group            { default = "" }
 variable default_cooldown           { default = 1 }
 variable desired_capacity           { default = "" }
 variable health_check_type          { default = "EC2" }
-variable vpc_zone_identifier        { default = "" }
+variable vpc_zone_identifier        { default = [] type = "list" }
 variable termination_policies       { default = "NewestInstance" }
 variable launch_configuration       {}
 variable health_check_grace_period  { default = 300 }
