2099daf7d67d74486abffbeabdd856cc8e2bd2d3
Aghogho A Omoware <a_omoware@hotmail.com>
2018-10-01 18:48:01 +0100

    user data might change

--- aws/ec2/launch_configuration/launch_configuration.tf
+++ aws/ec2/launch_configuration/launch_configuration.tf
@@ -9,6 +9,6 @@ resource "aws_launch_configuration" "lc" {
   iam_instance_profile  = "${var.iam_instance_profile}"
 
   lifecycle {
-     create_before_destroy = false
+     create_before_destroy = true
    }
 }
