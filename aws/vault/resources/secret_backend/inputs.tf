4ee8613cac54ee7d7c265d77fa70dcb651518033
Aghogho A Omoware <a_omoware@hotmail.com>
2018-10-01 16:00:11 +0100

    aws secret backend

---
+++ aws/vault/resources/secret_backend/inputs.tf
@@ -0,0 +1,8 @@
+variable access_key {}
+variable secret_key {}
+
+variable path { default = "" }
+variable region { default = "" }
+variable description { default = "" }
+variable default_lease_ttl_seconds { default = "" }
+variable max_lease_ttl_seconds { default = "" }
---
+++ aws/vault/resources/secret_backend/secret_backend.tf
@@ -0,0 +1,10 @@
+resource "vault_aws_secret_backend" "aws_backend" {
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+
+  path = "${var.backend_path}"
+  region = "${var.region}"
+  description = "${var.description}"
+  max_lease_ttl_seconds = "${var.max_lease_ttl_seconds}"
+  default_lease_ttl_seconds = "${var.default_lease_ttl_seconds}"
+}
