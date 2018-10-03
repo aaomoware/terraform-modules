e5f40a419a159eabfed1c1589fdcddbc2a182037
A A Omoware <a_omoware@hotmail.com>
2018-10-03 14:42:19 +0100

    Update secret_backend_role.tf
    lists of names  - group names

--- aws/vault/resources/secret_backend_role/secret_backend_role.tf
+++ aws/vault/resources/secret_backend_role/secret_backend_role.tf
@@ -1,5 +1,6 @@
 resource "vault_aws_secret_backend_role" "role" {
-  name    = "${var.name}"
+  count = "${length(var.name)}"
+  name    = "${element(var.name, count.index)}"
   backend = "${var.backend}"
   policy_arn  = "${var.policy_arn}"
 }
