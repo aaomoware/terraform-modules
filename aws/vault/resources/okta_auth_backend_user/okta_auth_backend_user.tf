660bec9dc367b264c464d1d0880b31c034835d84
A A Omoware <a_omoware@hotmail.com>
2018-10-03 18:05:11 +0100

    Update okta_auth_backend_user.tf
    many to one relationship

--- aws/vault/resources/okta_auth_backend_user/okta_auth_backend_user.tf
+++ aws/vault/resources/okta_auth_backend_user/okta_auth_backend_user.tf
@@ -1,6 +1,8 @@
 resource "vault_okta_auth_backend_user" "oabus" {
+    count = "${length(var.username)}"
+    
     path = "${var.path}"
     groups = "${var.groups}"
-    policies = "${var.policies}"
-    username = "${var.username}"
+    policies = ["${var.policies}"]
+    username = ["${element(var.username, count.index)}"]
 }
