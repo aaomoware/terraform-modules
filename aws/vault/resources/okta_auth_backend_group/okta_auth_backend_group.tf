0e39fa489c427b05092867cabd2ca895ba4e1e38
A A Omoware <a_omoware@hotmail.com>
2018-10-03 18:02:18 +0100

    Update okta_auth_backend_group.tf
    many to one relationship

--- aws/vault/resources/okta_auth_backend_group/okta_auth_backend_group.tf
+++ aws/vault/resources/okta_auth_backend_group/okta_auth_backend_group.tf
@@ -1,5 +1,7 @@
 resource "vault_okta_auth_backend_group" "oabg" {
+    count = "${length(var.group_name)}"
+    
     path = "${var.path}"
     group_name = "${element(var.group_name, count.index)}"
-    policies = ["${element(var.policies, count.index)}"]
+    policies = ["${var.policies}"]
 }
