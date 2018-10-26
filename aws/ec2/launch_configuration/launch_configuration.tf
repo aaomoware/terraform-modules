a637a57550de827bcf9dabdb55ed4cb05ab30a55
A A Omoware <a_omoware@hotmail.com>
2018-10-26 09:45:53 +0100

    associate_public_ip_address

--- aws/ec2/launch_configuration/launch_configuration.tf
+++ aws/ec2/launch_configuration/launch_configuration.tf
@@ -7,6 +7,7 @@ resource "aws_launch_configuration" "lc" {
   instance_type         = "${var.instance_type}"
   security_groups       = ["${var.security_groups}"]
   iam_instance_profile  = "${var.iam_instance_profile}"
+  associate_public_ip_address = "${var.associate_public_ip_address}"
 
   lifecycle {
      create_before_destroy = true
