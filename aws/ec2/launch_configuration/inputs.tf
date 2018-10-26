b9edec749b22738db3b864ef226742b643f38135
A A Omoware <a_omoware@hotmail.com>
2018-10-26 09:45:14 +0100

    associate_public_ip_address

--- aws/ec2/launch_configuration/inputs.tf
+++ aws/ec2/launch_configuration/inputs.tf
@@ -5,3 +5,4 @@ variable user_data            {}
 variable instance_type        {}
 variable security_groups      { type = "list" }
 variable iam_instance_profile {}
+variable associate_public_ip_address { default = false }
