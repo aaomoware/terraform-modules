47813c09b57bbd2f7e7119563f2cb02e1263547b
A A Omoware <a_omoware@hotmail.com>
2018-10-31 11:29:22 +0000

    defaulted

--- aws/ec2/launch_configuration/inputs.tf
+++ aws/ec2/launch_configuration/inputs.tf
@@ -1,7 +1,7 @@
 variable name_prefix          {}
 variable key_name             {}
 variable image_id             {}
-variable user_data            {}
+variable user_data            { default = "" }
 variable instance_type        {}
 variable security_groups      { type = "list" }
 variable user_data_base64     { default = "" }
