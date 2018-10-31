0fa109dadeeb162197a3da57943d5e48f0334720
A A Omoware <a_omoware@hotmail.com>
2018-10-31 11:53:27 +0000

    type must be one of [string, map, list] - 'type' is not a valid type

--- aws/eks/cluster/inputs.tf
+++ aws/eks/cluster/inputs.tf
@@ -3,5 +3,5 @@ variable create             { default = "20m" }
 variable delete             { default = "20m" }
 variable version            { default = "" }
 variable role_arn           { default = "" }
-variable subnet_ids         { default = [] type = "type" }
-variable security_group_ids { default = [] type = "type" }
+variable subnet_ids         { default = [] type = "list" }
+variable security_group_ids { default = [] type = "list" }
