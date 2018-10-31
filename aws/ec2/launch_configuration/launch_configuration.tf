c0976b5dc36fbd4887a4c803b7d2c16523c84fcb
A A Omoware <a_omoware@hotmail.com>
2018-10-31 11:23:49 +0000

    user_data_base64

--- aws/ec2/launch_configuration/launch_configuration.tf
+++ aws/ec2/launch_configuration/launch_configuration.tf
@@ -6,6 +6,7 @@ resource "aws_launch_configuration" "lc" {
   user_data             = "${var.user_data}"
   instance_type         = "${var.instance_type}"
   security_groups       = ["${var.security_groups}"]
+  user_data_base64      = "${var.user_data_base64}"
   iam_instance_profile  = "${var.iam_instance_profile}"
   associate_public_ip_address = "${var.associate_public_ip_address}"
 
