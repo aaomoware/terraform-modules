5580ff8f9a5344f144ac0f7aa7bb2f882c0d3ea0
A A Omoware <a_omoware@hotmail.com>
2018-10-31 12:57:32 +0000

    typo

--- aws/eks/cluster/outputs.tf
+++ aws/eks/cluster/outputs.tf
@@ -1,36 +1,36 @@
 
 output "id" {
-  vaule = "${aws_eks_cluster.ec.id}"
+  value = "${aws_eks_cluster.ec.id}"
 }
 
 output "arn" {
-  vaule = "${aws_eks_cluster.ec.arn}"
+  value = "${aws_eks_cluster.ec.arn}"
 }
 
 output "certificate_authority" {
-  vaule = "${aws_eks_cluster.ec.certificate_authority}"
+  value = "${aws_eks_cluster.ec.certificate_authority}"
 }
 
 output "certificate_authority_data" {
-  vaule = "${aws_eks_cluster.ec.certificate_authority.0.data}"
+  value = "${aws_eks_cluster.ec.certificate_authority.0.data}"
 }
 
 output "endpoint" {
-  vaule = "${aws_eks_cluster.ec.endpoint}"
+  value = "${aws_eks_cluster.ec.endpoint}"
 }
 
 output "platform_version" {
-  vaule = "${aws_eks_cluster.ec.platform_version}"
+  value = "${aws_eks_cluster.ec.platform_version}"
 }
 
 output "version" {
-  vaule = "${aws_eks_cluster.ec.version}"
+  value = "${aws_eks_cluster.ec.version}"
 }
 
 output "vpc_id" {
-  vaule = "${aws_eks_cluster.ec.vpc_config.vpc_id }"
+  value = "${aws_eks_cluster.ec.vpc_config.vpc_id }"
 }
 
 output "vpc_config" {
-  vaule = "${aws_eks_cluster.ec.vpc_config}"
+  value = "${aws_eks_cluster.ec.vpc_config}"
 }
