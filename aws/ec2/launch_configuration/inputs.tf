c3115ba1ac0b7fd4ef8b7e8adcf791ba32fe9246
A A Omoware <a_omoware@hotmail.com>
2018-10-31 11:24:58 +0000

    user_data_base64

--- aws/ec2/launch_configuration/inputs.tf
+++ aws/ec2/launch_configuration/inputs.tf
@@ -4,5 +4,6 @@ variable image_id             {}
 variable user_data            {}
 variable instance_type        {}
 variable security_groups      { type = "list" }
+variable user_data_base64     { default = "" }
 variable iam_instance_profile {}
 variable associate_public_ip_address { default = false }
