065c1a110e8b3aeed6d13f598b9fd1dcabdb9f2c
A A Omoware <a_omoware@hotmail.com>
2018-11-01 10:47:45 +0000

    "user_data": conflicts with user_data_base64

--- aws/ec2/launch_configuration/launch_configuration.tf
+++ aws/ec2/launch_configuration/launch_configuration.tf
@@ -1,4 +1,23 @@
+resource "aws_launch_configuration" "lc_base64" {
+  count = "${var.user_data_base64 == "" ? 0 : 1}"
+
+  name_prefix           = "${var.name_prefix}"
+  key_name              = "${var.key_name}"
+  image_id              = "${var.image_id}"
+  instance_type         = "${var.instance_type}"
+  security_groups       = ["${var.security_groups}"]
+  user_data_base64      = "${var.user_data_base64}"
+  iam_instance_profile  = "${var.iam_instance_profile}"
+  associate_public_ip_address = "${var.associate_public_ip_address}"
+
+  lifecycle {
+     create_before_destroy = true
+   }
+}
+
+
 resource "aws_launch_configuration" "lc" {
+  count = "${var.user_data == "" ? 0 : 1}"
 
   name_prefix           = "${var.name_prefix}"
   key_name              = "${var.key_name}"
