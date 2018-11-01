ef8a7985e951f870336c8aabeacc4ab848861e50
A A Omoware <a_omoware@hotmail.com>
2018-11-01 11:33:24 +0000

    debugging

--- aws/ec2/launch_configuration/launch_configuration.tf
+++ aws/ec2/launch_configuration/launch_configuration.tf
@@ -15,3 +15,20 @@ resource "aws_launch_configuration" "lc_base64" {
    }
 }
 
+resource "aws_launch_configuration" "lc" {
+  count = "${var.user_data != "" ? 1 : 0}"
+
+  name_prefix           = "${var.name_prefix}"
+  key_name              = "${var.key_name}"
+  image_id              = "${var.image_id}"
+  user_data             = "${var.user_data}"
+  instance_type         = "${var.instance_type}"
+  security_groups       = ["${var.security_groups}"]
+  user_data_base64      = "${var.user_data_base64}"
+  iam_instance_profile  = "${var.iam_instance_profile}"
+  associate_public_ip_address = "${var.associate_public_ip_address}"
+
+  lifecycle {
+     create_before_destroy = true
+   }
+}
