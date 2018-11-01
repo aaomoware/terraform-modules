6cd1a012f99f57a02a4907aabb852e9c0e6115d2
A A Omoware <a_omoware@hotmail.com>
2018-11-01 14:01:09 +0000

    Update inputs.tf

--- aws/vpc/route_igw/inputs.tf
+++ aws/vpc/route_igw/inputs.tf
@@ -1,4 +1,4 @@
 variable igw_count                 { default = "0" }
-variable gateway_id                { default = [] type = "list" }
+variable gateway_id                { default = "list" }
 variable route_table_id            { default = [] type = "list" }
 variable destination_cidr_block    { default = "" }
