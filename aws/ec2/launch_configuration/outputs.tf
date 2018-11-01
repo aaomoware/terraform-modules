bf1c87dd0dfc68710856eb75b7831152041e7eb0
A A Omoware <a_omoware@hotmail.com>
2018-11-01 11:35:09 +0000

    lc_base64 id output

--- aws/ec2/launch_configuration/outputs.tf
+++ aws/ec2/launch_configuration/outputs.tf
@@ -1,3 +1,7 @@
 output "id" {
   value = "${element(concat(aws_launch_configuration.lc.*.id, list("")), 0)}"
 }
+
+output "id_base64" {
+  value = "${element(concat(aws_launch_configuration.lc_base64.*.id, list("")), 0)}"
+}
