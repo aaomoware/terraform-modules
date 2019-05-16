932ce0c7ce9223d60e22bb061e14a25bd72f3778
Aghogho A Omoware <a_omoware@hotmail.com>
2019-05-16 12:59:20 +0100

    typo

--- aws/iam/iam_role_policy_attachment/iam_role_policy_attachment.tf
+++ aws/iam/iam_role_policy_attachment/iam_role_policy_attachment.tf
@@ -2,5 +2,5 @@ resource "aws_iam_role_policy_attachment" "irpa" {
   count = "${length(var.role)}"
 
   role       = "${element(var.role, count.index)}"
-  policy_arn = "${var.policy_arn[element(var.role, count.index)])}"
+  policy_arn = "${var.policy_arn[element(var.role, count.index)]}"
 }
