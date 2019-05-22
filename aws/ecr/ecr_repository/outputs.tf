91730d64114492a1c5b6a2c496c33d67bf3ace37
Aghogho A Omoware <a_omoware@hotmail.com>
2019-05-22 14:04:54 +0100

    remove ouput

--- aws/ecr/ecr_repository/outputs.tf
+++ aws/ecr/ecr_repository/outputs.tf
@@ -4,9 +4,6 @@ output "arn" {
 output "name" {
   value = "${aws_ecr_repository.ecr_repo.name}"
 }
-output "registry_id" {
-  value = "${aws_ecr_repository.ecr_repo.repository}"
-}
 output "repository_url" {
   value = "${aws_ecr_repository.ecr_repo.repository_id}"
 }
