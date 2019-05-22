fcc7eb4fcd3c76ab67f4f743ec6c44a3423ec014
Aghogho A Omoware <a_omoware@hotmail.com>
2019-05-22 14:08:07 +0100

    remove ouput

--- aws/ecr/ecr_repository/outputs.tf
+++ aws/ecr/ecr_repository/outputs.tf
@@ -5,5 +5,5 @@ output "name" {
   value = "${aws_ecr_repository.ecr_repo.name}"
 }
 output "repository_url" {
-  value = "${aws_ecr_repository.ecr_repo.repository_id}"
+  value = "${aws_ecr_repository.ecr_repo.repository_url}"
 }
