7bd4379fea3476aafbd38b49e065bd031bea7b4f
Aghogho A Omoware <a_omoware@hotmail.com>
2019-05-22 17:08:58 +0100

    removed

--- aws/ecr/ecr_repository_policy/outputs.tf
+++ aws/ecr/ecr_repository_policy/outputs.tf
@@ -1,7 +1,3 @@
 output "repository" {
   value = "${aws_ecr_lifecycle_policy.ecr_lc_policy.repository}"
 }
-
-output "repository_id" {
-  value = "${aws_ecr_lifecycle_policy.ecr_lc_policy.repository_id}"
-}
